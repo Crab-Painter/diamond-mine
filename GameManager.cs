3649a09b3a8e68b8bd4a6cd5a75b6dd42c98deb4
Octosaur <hund12359@gmail.com>
2026-03-06 22:30:20 +0200

    end of day commit
    detection of cards under the click, moving to drag itself

--- GameManager.cs
+++ GameManager.cs
@@ -1,8 +1,16 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
-public partial class GameManager : Node
+public partial class GameManager : Node2D
 {
+	public enum GameStates
+	{
+		@default,
+		dragCard
+	}
+	private Node draggedCardNode;
+	private Node state;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -17,9 +25,29 @@ public partial class GameManager : Node
 
 	public override void _Input(InputEvent @event)
 	{
-		if (@event.)
+		if (@event.IsActionPressed("LMB"))
 		{
+			// PointTracing();
+			var spaceState = GetWorld2D().DirectSpaceState;
+			PhysicsPointQueryParameters2D queryParams = new();
+			queryParams.SetPosition(GetGlobalMousePosition());
+			queryParams.SetCollideWithAreas(true);
+			queryParams.SetCollisionMask(1);
+			int resultLimit = 1;
+			// as far as i understand, matches sorted in Z order. Since we need only top one, limiting number of results will improve performance
+    		var matches = spaceState.IntersectPoint(queryParams,resultLimit);
+			var result = matches.Count > 0 ? matches[0] : null;
+			CollisionObject2D collider = (CollisionObject2D)(GodotObject)result["collider"];
+			Node cardNode = collider.GetParent();
+			draggedCardNode = cardNode;
+			state = 
 			
+			GD.Print(cardNode);
+		}
+
+		if (@event.IsActionReleased("LMB"))
+		{
+			// GD.Print("released");
 		}
 	}
 }
