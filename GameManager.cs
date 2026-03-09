af51007f7770e3282f2efcea5a2ad471548fa470
Octosaur <hund12359@gmail.com>
2026-03-09 14:47:04 +0200

    todo version of moving cards
    debug logs included. Need to switch to a card generation due to technical reasons atm

--- GameManager.cs
+++ GameManager.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 public partial class GameManager : Node2D
 {
@@ -9,8 +10,8 @@ public partial class GameManager : Node2D
 		@default,
 		dragCard
 	}
-	private Node draggedCardNode;
-	private Node state;
+	private Node2D draggedCardNode;
+	private GameStates state = GameStates.@default;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -20,33 +21,64 @@ public partial class GameManager : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		if (state == GameStates.dragCard)
+		{
+			Vector2 mousePosition = GetGlobalMousePosition();
+			draggedCardNode.Position = new Vector2(
+				float.Clamp(mousePosition.X, 0, GetViewportRect().Size.X),//looks like this might cause productivity drop?
+				float.Clamp(mousePosition.Y, 0, GetViewportRect().Size.Y)
+			);
+		}
 	}
 
 	public override void _Input(InputEvent @event)
 	{
 		if (@event.IsActionPressed("LMB"))
 		{
-			// PointTracing();
 			var spaceState = GetWorld2D().DirectSpaceState;
 			PhysicsPointQueryParameters2D queryParams = new();
 			queryParams.SetPosition(GetGlobalMousePosition());
 			queryParams.SetCollideWithAreas(true);
 			queryParams.SetCollisionMask(1);
-			int resultLimit = 1;
+			int resultLimit = 16;
 			// as far as i understand, matches sorted in Z order. Since we need only top one, limiting number of results will improve performance
+			//TODO check this
     		var matches = spaceState.IntersectPoint(queryParams,resultLimit);
-			var result = matches.Count > 0 ? matches[0] : null;
-			CollisionObject2D collider = (CollisionObject2D)(GodotObject)result["collider"];
+			if (matches.Count == 0)
+			{
+				return;
+			}
+
+			GD.Print("Get " + matches.Count + " collisions");
+			//get the top card (highest absolute Z index)
+			CollisionObject2D collider = (CollisionObject2D)(GodotObject)matches[0]["collider"];
+			int maxZId = ((Node2D)collider.GetParent()).ZIndex;
+			GD.Print("Starting node is" + collider.GetParent().Name + ". It's index is " + maxZId);
+			foreach (Godot.Collections.Dictionary match in matches)
+			{
+				CollisionObject2D collisionNode = (CollisionObject2D)(GodotObject)match["collider"];
+				GD.Print("Checking node is" + collisionNode.GetParent().Name + ". It's index is " + ((Node2D)collisionNode.GetParent()).ZIndex);
+				if (((Node2D)collisionNode.GetParent()).ZIndex > maxZId)
+				{
+					GD.Print("new collider candidate");
+					maxZId = ((Node2D)collisionNode.GetParent()).ZIndex;
+					collider = collisionNode;
+				}
+				
+			}
+
+			GD.Print("Collider node is" + collider.GetParent().Name + ". It's index is " + maxZId);
 			Node cardNode = collider.GetParent();
-			draggedCardNode = cardNode;
-			state = 
+			draggedCardNode = (Node2D)cardNode;//to ensure that we can move a card through script its root node should be child of or Node2D
+			//TODO when i start moving to cards i need to ensure that root node is node2d
+			state = GameStates.dragCard;
 			
-			GD.Print(cardNode);
+			GD.Print(cardNode.Name);
 		}
 
 		if (@event.IsActionReleased("LMB"))
 		{
+			state = GameStates.@default;
 			// GD.Print("released");
 		}
 	}
